1f124aba0772b15720a4c4dd2508120712631d35
zpf0918 <zhanggongzifei@163.com>
2017-07-17 10:12:35 +0800

    rspec validate_end_at_with_amount

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Parking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+   describe ".validate_end_at_with_amount" do
+
+     it "is invalid without amount" do
+       parking = Parking.new( :parking_type => "guest",
+                              :start_at => Time.now - 6.hours,
+                              :end_at => Time.now)
+       expect( parking ).to_not be_valid
+     end
+
+     it "is invalid without end_at" do
+       parking = Parking.new( :parking_type => "guest",
+                              :start_at => Time.now - 6.hours,
+                              :amount => 100)
+       expect( parking ).to_not be_valid
+     end
+   end
+
 end
