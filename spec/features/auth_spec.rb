22babe086b44e74912cabe577b5eb5437410b2a9
zpf0918 <zhanggongzifei@163.com>
2017-07-17 14:29:17 +0800

    test login and logout

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -21,4 +21,23 @@ feature "register and login", :type => :feature do
     user = User.last
     expect(user.email).to eq("foobar@example.com")
   end
+
+  scenario "login and logout" do
+    user = User.create!( :email => "test@12.com", :password => "123456")
+
+    visit "/users/sign_in"
+
+    within("#new_user") do
+      fill_in "Email", with: "test@12.com"
+      fill_in "Password", with: "123456"
+    end
+
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
+
+    click_link "登出"
+
+    expect(page).to have_content("Signed out successfully")
+  end
 end
