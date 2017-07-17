372e30b6334e5b57c24eccba2886832bb0dedc30
zpf0918 <zhanggongzifei@163.com>
2017-07-17 13:26:12 +0800

    redefined calculate_amount

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -19,13 +19,38 @@ class Parking < ApplicationRecord
   end
 
   def calculate_amount
-    factor = (self.user.present?)? 50:100
     if self.amount.blank? && self.start_at.present? && self.end_at.present?
-      if duration <= 60
-        self.amount = 200
-      else
-        self.amount = 200 + ( (duration - 60 ).to_f / 30).ceil * factor
+      if self.user.blank?
+        self.amount = calculate_guest_term_amount
+      elsif self.parking_type == "short-term"
+        self.amount = calculate_short_term_amount
+      elsif self.parking_type == "long-term"
+        self.amount = calculate_long_term_amount
       end
+    end
+  end
+
+  def calculate_guest_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
+    end
+  end
+
+  def calculate_short_term_amount
+    if duration <= 60
+      self.amount = 200
+    else
+      self.amount = 200 + ((duration - 60).to_f / 30).ceil * 50
+    end
+  end
+
+  def calculate_long_term_amount
+    if duration <= 720
+      self.amount = 1200
+    elsif duration > 720 && duration <= 1440
+      self.amount = 1600
     end
   end
 end
